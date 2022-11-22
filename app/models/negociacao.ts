74e1e4df309daa724ea0c85ae33988a3be8d54b4
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-11-21 21:50:32 -0300

    refactor: refactoring js to ts

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -2,23 +2,33 @@
 // Eu poderia usar a tralha antes dos atributos, mas seria mais usado no JS.
 // No TS é melhor usar a forma private, e por convenção, não é obrigação, se coloca underline antes do atributo
 // já pra indicar que é privado.
+
 export class Negociacao {
-    constructor(data, quantidade, valor) {
+    private _data: Date;
+    private _quantidade: number;
+    private _valor: number;
+
+    constructor(data: Date, quantidade: number, valor: number) {
         this._data = data;
         this._quantidade = quantidade;
         this._valor = valor;
     }
+
     // Vou criar getters para poder enxergar os atributos, uma vez que eu fiz eles privados com a tralha #
     get data() {
         return this._data;
     }
+
     get quantidade() {
         return this._quantidade;
     }
+
     get valor() {
         return this._valor;
     }
+
     get volume() {
         return this._quantidade * this._valor;
     }
+
 }
