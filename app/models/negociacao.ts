ae2581ea023517194922f9e6e20346c4f3c23d71
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-11-23 10:12:13 -0300

    refactor: tipando metodos

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -15,19 +15,19 @@ export class Negociacao {
     }
 
     // Vou criar getters para poder enxergar os atributos, uma vez que eu fiz eles privados com a tralha #
-    get data() {
+    get data(): Date {
         return this._data;
     }
 
-    get quantidade() {
+    get quantidade(): number {
         return this._quantidade;
     }
 
-    get valor() {
+    get valor(): number {
         return this._valor;
     }
 
-    get volume() {
+    get volume(): number {
         return this._quantidade * this._valor;
     }
 
